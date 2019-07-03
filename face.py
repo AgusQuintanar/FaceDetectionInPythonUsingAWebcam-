915bb8aa7197cfde3c61ebffe68c4232216a03a1
AgusQuintanar <agusquintanar17@gmail.com>
2019-07-02 21:13:42 -0500

    Added test image and xml files

--- face.py
+++ face.py
@@ -1,16 +1,16 @@
-  import cv2
-    import sys
-    image=sys.argv[1]
+import cv2
+import sys
+image=sys.argv[1]
 
-   face_cascade = cv2.CascadeClassifier(‘data/haarcascade_frontalface_default.xml’)
+face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
 
-    img = cv2.imread(image)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.imread(image)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+faces = face_cascade.detectMultiScale(gray, 1.3, 4)
 
-    for (x, y, w, h) in faces:
-      cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-    cv2.imshow(‘img’, img)
-    cv2.waitKey()
+cv2.imshow('img', img)
+cv2.waitKey()
